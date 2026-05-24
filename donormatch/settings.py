24a3ce35a96fd03cd4f8808b2f892ffb5ce84f6d
eugene <monietrix@gmail.com>
2026-05-24 18:45:27 +0100

    donor_match

--- donormatch/settings.py
+++ donormatch/settings.py
@@ -87,6 +87,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ['*']
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
